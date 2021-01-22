8adb94caa49154eed62a58cb0c616f6841020ee1
Justin Davis <jdavis@trellahealth.com>
2021-01-22 10:06:15 -0500

    Comments and questions added

--- pharmapacks/example_code_2_documented.cs
+++ pharmapacks/example_code_2_documented.cs
@@ -13,31 +13,44 @@ using System.Text.RegularExpressions;
 
 namespace PrinterManager.Server
 {
+    /// <summary>Start and manage a TCP server to handle ZPL-print requests sent over the network.</summary>
     public class TcpServerAsyncWebsockets
     {
+        // We default to using the local host as our TCP listener.
         IPAddress host = IPAddress.Parse("127.0.0.1");
+        // QUESTION/COMMENT- If this is a number that is a convention for us, I'd like to put it in a constant or config. 
         const int PORT = 1516;
         private readonly TcpListener server;
 
+        /// <summary>Start a TCP listener at the default host IP and port and set up a databaes connection for it.</summary>
+        // QUESTION/COMMENT - We don't actually seem to use databaseAccess for anything, but maybe in real code we'd use it
+        // when logging, or pass it to the Printer class to tell it where to look for printers? I'd want to explain that if 
+        // possible. 
         public TcpServerAsyncWebsockets()
         {
             this.server = new TcpListener(host, PORT);
             DatabaseSetup databaseAccess = new DatabaseSetup();
         }
 
+        /// <summary>Start the TCP server and get ready to handle print requests.</summary>
         public void ServerStart()
         {
             server.Start();
             AcceptConnection();
         }
 
+        // Set the delegate method that the server will use to handle print requests it receives. 
         private void AcceptConnection()
         {
             server.BeginAcceptTcpClient(HandleConnection, server);
         }
 
+        // The delegate that processes incoming print requests and, if possible, prints them.
         private void HandleConnection(IAsyncResult result)
         {
+            // This call doesn't make sense. Isn't this the method we called to pass this method as a delegate?
+            // So if we're in this method, we know we had to have successfully called AcceptConnection() already, 
+            // and that method doesn't do anything besides specify this method. Why are we calling this? 
             AcceptConnection();
             TcpClient client = server.EndAcceptTcpClient(result);
 
@@ -45,48 +58,111 @@ namespace PrinterManager.Server
             {
                 NetworkStream ns = client.GetStream();
 
+                // QUESTION/COMMENT- I'm not sure why we have this while loop. It would make sense if we were reading incoming data in a synchronous way, but 
+                // the rest of the code acts like result has all the data we're ever going to get. 
                 while (true)
                 {
+                    // QUESTION/COMMENT- Silly questions, but ones any coder will ask - if we never start getting data
+                    // in stream for some reason, or the client only sends two bytes of data for some weird
+                    // reason, these loops are infinite. Is that a problem?
+                    // The space between the parentheses and the semicolon looks deliberate, given the rest of 
+                    // the formatting, so I'm taking that for a convention to indicate an empty while loop. 
                     while (!ns.DataAvailable) ;
+
+                    // QUESTION/COMMENT- Below, is 3 a magic value for us in some way? If so, I'd like to replace with a named const. 
+                    // From reading the next lines of code, it seems like once we get the data, we get it all - when we
+                    // assign bytesCount, we never check again to see if it has changed - so what's up for us that we're not 
+                    // saying "while (client.Available == 0)"?
+                    // COMMENT - Available is the number of bytes of data received from the network and available to be read.
+                    // Wait until we have some until starting work. 
                     while (client.Available < 3) ;
 
+                    // incomingData holds all the data we got in the print request, which includes metadata as well as the 
+                    // data that we want to print. 
                     byte[] incomingData = new byte[client.Available];
                     int bytesCount = ns.Read(incomingData, 0, client.Available);
+
+                    // We only use decodedData to check for a handshake request from the client. For an actual print request, 
+                    // we just use the incomingData byte array. 
                     string decodedData = Encoding.UTF8.GetString(incomingData);
 
+                    // If the data we received starts with GET (case-insensitive), then just
+                    // send a handshake response to the client. Don't do any other processing.
                     if (Regex.IsMatch(decodedData, "^GET", RegexOptions.IgnoreCase))
                     {
                         byte[] response = MakeHandshake(decodedData);
                         ns.Write(response, 0, response.Length);
                     }
+                    // It wasn't a handshake request - it's a real print request. 
                     else
                     {
+                        // We do a lot of low-level bit operations in this block to get and interpret the control bytes
+                        // from the client request. 
+
+                        // First, we do a bitwise comparison on the first two bytes of data to figure out if the fin and mask flags
+                        // have been set. 
+                        // 0b at the beginning of a number tells C# that the rest of the number are literal bits.
+                        // fin is true if the first bit of the first incoming byte is 1, 
+                        // mask is true if the first bit of the second incoming byte is 1.
+                        // If the mask bit isn't set, we're not going to print anything. 
+
+                        // QUESTION/COMMENT- We never use fin. Why do we bother getting it?
                         bool fin = (incomingData[0] & 0b10000000) != 0,
                             mask = (incomingData[1] & 0b10000000) != 0; 
 
+                        // The last 4 bits of the first incoming byte give us the numeric value for opcode. 
+                        // We strip the first 4 bits by AND-ing them with 0s. 
                         int opcode = incomingData[0] & 0b00001111, 
+                            // The last seven bits of the second incoming byte give us our message length.
+                            // We can find out what the last seven bits are by subtracting the 8th bit, which is worth 128
+                            // if it's 1. That leaves us with a message length of 0-127 bytes. 
                             msglen = incomingData[1] - 128, 
+                            // by default we assume that the first two bytes are metadata and the data starts at byte 3
+                            // (0-indexed)
                             offset = 2;
 
-                        if (msglen == 126)
+                        // If the rightmost bit of the message length byte is 0 and the six bits to the right of that are 1's, 
+                        // (126), that's a flag that the msglen is actually 
+                        // a number that is two bytes long, that it is stored in bytes 3 and 4, and that our print data starts at byte, not byte 3.
+                        // QUESTION/COMMENT- 126 might make a good named constant. 
+                        if (msglen == 126) 
                         {
+                            // The 0 argument just means start reading at the first (0-index) byte of the byte array we pass in the first argument.
                             msglen = BitConverter.ToUInt16(new byte[] { incomingData[3], incomingData[2] }, 0);
                             offset = 4;
                         }
-
+                        // QUESTION/COMMENT- Is this an error? We went to some trouble in the lines above to get a two-byte message length,
+                        // and an offset value that works with it, but with the "else" below, we're never going to use it. 
                         else if (mask)
                         {
+                            // Get an array of msglen bytes to hold our print data.
                             byte[] decoded = new byte[msglen];
+                            // The four bytes after the bytes with the opcode, msglen, fin and mask values are data masks that we'll use to 
+                            // transform the data we read. The print data actually starts after the mask bytes. 
                             byte[] masks = new byte[4] { incomingData[offset], incomingData[offset + 1], incomingData[offset + 2], incomingData[offset + 3] };
                             offset += 4;
 
+                            // QUESTION/COMMENT- is there a reason we use ++i in the for loop below? As far as I can tell it has the same effect as i++, but people are 
+                            // really used to seeing i++ and when they see ++i, it makes them stop and wonder why and look for something they're missing. 
+                            // If we just like using ++i, that's fine, I'm just offering my two cents from a training and onboarding perspectie. 
+
+                            // COMMENT - read all of the actual incoming data, starting at the offset. 
+                            // We use the mask bytes to transform the byte data we received and which we will ultimately deserialize below into
+                            // an input object.
+                            // To transform the data, as we read each data byte, we apply a bitwise exclusive OR operation (true if the bits compared are different,
+                            // false otherwise) to the data byte and one of the mask bytes. We apply the mask bytes to the data bytes in groups of four. 
+                            // The first data byte we read gets the first mask byte, the second data byte the second mask byte, etc., then we start again
+                            // with the fifth data byte getting the first mask byte. 
                             for (int i = 0; i < msglen; ++i)
                                 decoded[i] = (byte)(incomingData[offset + i] ^ masks[i % 4]);
 
+                            // Turn our transformed data byte array into a JSON string.
                             string text = Encoding.UTF8.GetString(decoded);
 
+                            // The output HTTP response we'll send back to the client. 
                             string writeJson;
 
+                            // Finally, we have can turn our input JSON string into an input object.
                             ClientInput input = JsonConvert.DeserializeObject<ClientInput>(text);
 
                             if (!input.Authenticate())
@@ -97,6 +173,7 @@ namespace PrinterManager.Server
                                 break;
                             }
 
+                            // QUESTION/COMMENT- "print" might be a good named constant. 
                             if (input.command != "print")
                             {
                                 writeJson = Status.Create(400, "Bad Request: Incorrect command. Please try again.");
@@ -105,6 +182,7 @@ namespace PrinterManager.Server
                                 break;
                             }
 
+                            // Get a Zebra Print Language (ZPL) string from the input and validate it.
                             ZplString zpl = new ZplString(input.zpl);
 
                             if (!zpl.Validate())
@@ -125,8 +203,12 @@ namespace PrinterManager.Server
                                 break;
                             }
 
+                            // Finally, we print to the printer we found in the new Printer() call above and log the whole interaction.
                             printer.Print(zpl.text);
 
+                            // QUESTION/COMMENT- do we want to log errors? The way the code is written, we break on any error that we know how to 
+                            // handle, like invalid ZPL data or printer not found, so we'll never see errors in our log. Exceptions go to the 
+                            // ExceptionLogRepository, but errors we know how to handle are just lost. 
                             Dictionary<String, String> logEntry = new Dictionary<String, String>
                                 {
                                     {"IPAddress", SystemHelper.GetLocalIP()},
@@ -140,8 +222,14 @@ namespace PrinterManager.Server
                             writeJson = Status.Create(200, "OK: Action completed");
                             ns.Write(Encoding.Default.GetBytes(writeJson), 0, Encoding.Default.GetBytes(writeJson).Length);
                             client.Close();
+
+                            // QUESTION/COMMENT- We don't have a break here, so we stay in while(true). Is that what we want to do?
+                            // We just closed the client, which we instantiated just to handle the result passed to this method, 
+                            // so it doesn't seem like there's anything more for the while loop to do. 
                         }
                         else
+                            // QUESTION/COMMENT- the code reads a little easier if you put a quick exit condition like this before all
+                            // the lengthy data processing logic. 
                             Console.WriteLine("mask bit not set");
 
                         Console.WriteLine();
@@ -154,6 +242,9 @@ namespace PrinterManager.Server
             }
         }
 
+        // Returns an array that the server can send to a client that has requested a websocket handshake. 
+        // Documentation for this process is available here: 
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Sec-WebSocket-Accept
         private byte[] MakeHandshake(string message)
         {
             Console.WriteLine("=====Handshaking from client=====\n{0}", message);
--- pharmapacks/example_code_documented.cs
+++ pharmapacks/example_code_documented.cs
@@ -7,35 +7,93 @@ using PrinterManager.Database.Repositories;
 
 namespace PrinterManager.Printing
 {
+    // Triple-slashed XML comments with reserved keywords lend themsevles to auto-generated documentation
+    // (see here: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/xmldoc/). Even if you don't 
+    // plan now on auto-generating the documentation, it's a nice, readable format for comments and you're keeping your options open.
+    // Generally I see them on public members, and that's what a lot of style guide software enforces, but I'll drop them on the internal
+    // classes here. 
+
+    /// <summary>
+    /// Retrieves, or if necessary configures, a default printer and then allows consumers to print Zebra Printer Language (ZPL)-formatted data to it. 
+    /// </summary>
+    /// <remarks>See here for an overview of the Zebra Printer Language (ZPL): 
+    /// https://www.zebra.com/us/en/support-downloads/knowledge-articles/zpl-command-information-and-details.html
+    /// </remarks>
     class Printer
+    // QUESTION/COMMENT - It's a little more readable to spell out that the access modifier for the class above is "internal" 
+    // and spare someone a Google search for the default value.
     {
+        // The name of the default printer we found or set, which is also the only printer this class will use. 
         String defaultPrinter = "";
 
+        /// <summary>Create a new printer instance and set it to use the default printer or have it set a default printer if there isn't one.</summary>
+        /// <remarks>
+        /// The constructor will get the name of the default printer set in the printer repository. If there isn't one,
+        /// it will search the list of installed printers for the first Zebra-named printer it can find and set that as
+        /// the default printer in the printer repository. 
+        /// </remarks>
         public Printer()
         {
             this.SetDefaultPrinter();
         }
 
+        /// <summary>Get the name of the printer the instance is using to print. </summary>
+        // QUESTION/COMMENT - You might want to call this GetDefaultPrinterName(), since that's the only printer you're ever going to see.
         public String GetName()
         {
             return defaultPrinter;
         }
 
+        /// <summary>Prints a ZPL-formatted string to the default printer.</summary>
+        /// <remarks>See here for an overview of the Zebra Printer Language (ZPL): 
+        /// https://www.zebra.com/us/en/support-downloads/knowledge-articles/zpl-command-information-and-details.html
+        /// </remarks>
+        // QQUESTION/COMMENT - what happens if we coudln't find an installed Zebra printer? Will SendStringToPrinter() throw an exception?
+        // That's good to note. 
         public void Print(String zplString)
         {
+            
             PrintHelper.SendStringToPrinter(this.defaultPrinter, zplString);
         }
 
+        // QUESTION/COMMENT - If part of what you're concerned about is code readability for new programmers coming on, then I think 
+        // SetDefaultPrinter() could use a better name. Usually when you have a method that's called something like SetX, you're able to pass a 
+        // value that X will take on. This method doesn't give the user any ability to say what the default printer should be.
+        // Instead it discovers the configured default printer, if there is one, and otherwise tries to set the first valid Zebra printer
+        // in its list as the default. 
+        // GetDefaultPrinter() isn't a great name either since if there isn't a default printer configured, the code tries to set one
+        // in the printer repository, which seems like it is an external data source where that change will live on past this class instance. 
+        // A lot of people would not expect a Get method to have any data side-effects. 
+        // RetrieveOrImplicitlySetDefaultPrinter() is a clunky name but it is accurate. 
+
+        // Sets this instance to use the printer configured in the repository as the default. If no default is found,
+        // it sets the first Zebra-named printer in the list of installed printers as the default printer in the repository 
+        // and then uses that printer. 
         private void SetDefaultPrinter()
         {
+            // Get the name of the default printer, if one has been configured, from the printer database repositorty. 
+            // Question - could SettingsRepository.Get() return a null? 
             String printer = SettingsRepository.Get("DefaultPrinter");
 
+            // If there is a default printer name and it's a valid name, then set it as the default printer
+            // and get out. Otherwise, keep going. 
             if (printer != "" && this.Validate(printer))
             {
                 this.defaultPrinter = printer;
                 return;
             }
 
+            // If we got this far, there was no default printer configured, so we're going to set one. 
+            // Loop through all the installed printers. If you find one whose name contains "zebra" 
+            // and whose name is also generally valid, set that as the default printer for this class
+            // instance and then save it to the printer settings repository as the default printer. 
+            // Zebra printers are (presumably) our stock printer brand. 
+
+            // If there is no printer with a valid name that contains the string "zebra," then we will not
+            // configure any default printer. 
+            // Should that be an exception? There's no way in this class to specify a printer to use and the Print()
+            // method presumes that you do have a default printer configured. If there's no default configured
+            // and the class can't find one using the logic below, the class instance will essentially be broken.
             foreach (String printerName in this.GetInstalledPrinters())
             {
                 if (printerName.ToLower().Contains("zebra") && this.Validate(printerName))
@@ -47,16 +105,38 @@ namespace PrinterManager.Printing
             }
         }
 
+        /// <summary>Deterime if the printer specified by printerName is a valid printer to use for printing.</summary>
+        // QUESTION/COMMENT - I can tell you what this method does, but not why, so I'd have some questions about that. Please see below.
         private bool Validate(String printerName)
         {
+            // QUESTION/COMMENT - I'd like to figure out why we set printer.PrinterName to printerName, so I could explain that. Maybe doing so means 
+            // that when we check printer.IsValid, PrinterSettings will apply some additional logic of its own in determing if the  
+            // printer called printerName is in fact a valid printer.
             PrinterSettings printer = new PrinterSettings();
             printer.PrinterName = printerName;
 
+            // QUESTION/COMMENT - This is an area where I'd want to do some digging if this were real code, to explain where that magic string comes from and what it means. 
+            // It looks from the order of the code like objsearcher needs the connection that mgtscope creates, but since those two objects don't interact 
+            // in any obvious way I'm not sure how objsearcher is finding out about that connection. I'd want to know more about that.  
             ManagementScope mgmtscope = new ManagementScope(@"\root\cimv2");
             mgmtscope.Connect();
+            // QUESTION/COMMENT - Do we need to worry about terminating the connection above? Looks like it's talking to an external resource.
+
+            // QUESTION/COMMENT - I can tell you what the code below is doing, but I can't tell you why, so I have some questions about that. 
+            // The code looks like it is getting a list of every printer whose name contains printerName anywhere within it. printerName will be
+            // a printer from the list of installed printers whose name contains "zebra" (case-insensitive). The code here is getting a list of
+            // printers from a different source than the GetInstalledPrinters() method, and I'm not sure what's different about the two 
+            // different printer lists. 
+            // Once the code gets the list of printers from its ManagemenObject query, it loops through it 
+            // and if it finds any printer in the list with a WorkOffline property that is true, 
+            // then it has the Validate() method return false. I guess if a printer can work offline, or has to work offline, you don't want it 
+            // to be used as the default? Though any given printer you're looking at in the list return by the ManagementObject query might 
+            // not even be the same printer as the one specified by printerName. 
+            // We're just getting the management object list of printers from a wildcard search on printerName. That confuses me.
             ManagementObjectSearcher objsearcher = new ManagementObjectSearcher("Select * from Win32_Printer where Name like '%" + printerName + "%'");
             foreach (ManagementObject prntr in objsearcher.Get())
             {
+                // QUESTION/COMMENT - We never use t. Why do we even declare it? It's not for the informative name.
                 String t = prntr["WorkOffline"].ToString();
                 if (prntr["WorkOffline"].ToString().Equals("true"))
                 {
@@ -64,17 +144,21 @@ namespace PrinterManager.Printing
                 }
             }
 
+            // QUESTION/COMMENT - I'd want to do a little research to figure out what the criteria for being valid are, and maybe mention them. 
             return printer.IsValid;
         }
 
+        /// <summary>Return a list of the names of all installed printers.</summary>
         private List<String> GetInstalledPrinters()
         {
             List<String> installedPrinters = new List<String>();
-
+            
             String printer;
 
+            // QUESTION/COMMENT - If we can use PrinterSettings here without instantiating it, then 
+            // why do we instantiate it in Validate() above? 
             for (int i = 0; i < PrinterSettings.InstalledPrinters.Count; i++)
-            {
+            { 
                 printer = PrinterSettings.InstalledPrinters[i];
                 installedPrinters.Add(printer);
             }
